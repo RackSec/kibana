d506e1f5d1171883e17e1af78fbe47cee4d9600a
Oskar Zabik <oskar.zabik@gmail.com>
2015-12-08 14:18:11 +0100

    addig capitalizeFirstLetter filter

--- src/app/filters/all.js
+++ src/app/filters/all.js
@@ -130,4 +130,12 @@ define([
     };
   });
 
+  module.filter('capitalizeFirstLetter', function() {
+    return function(input, scope) {
+      if (input!=null)
+      input = input.toLowerCase();
+      return input.substring(0,1).toUpperCase()+input.substring(1);
+    }
+  })
+
 });
