0b0f6747879678e08a92aacf9fbcc19dffe6a1c6
Oskar Zabik <oskar.zabik@gmail.com>
2015-12-09 16:01:24 +0100

    check if input is defined instead of checking for null

--- src/app/filters/all.js
+++ src/app/filters/all.js
@@ -131,10 +131,11 @@ define([
   });
 
   module.filter('capitalizeFirstLetter', function() {
-    return function(input, scope) {
-      if (input!=null)
-      input = input.toLowerCase();
-      return input.substring(0,1).toUpperCase()+input.substring(1);
+    return function(input) {
+      if(!(_.isUndefined(input))) {
+        input = input.toLowerCase();
+        return input.substring(0,1).toUpperCase()+input.substring(1);
+      }
     }
   })
 
